d6695b51c628983a538cadf47b77bbcf739d58e8
Brian Hopkinson <bmhopkin@uga.edu>
2021-11-17 16:48:38 -0500

    prototype rnn version of nviewnet

--- training_testing/nviewnet_v2.py
+++ training_testing/nviewnet_v2.py
@@ -28,3 +28,47 @@ class nViewNet(nn.Module):
         pooled = self.collapse(base_images)
         pooled = self.fc(pooled)
         return pooled
+        
+        
+class nViewNet_RNN(nn.Module):
+    def __init__(self, base_model, num_classes, num_views):
+        super().__init__()  
+        self.base = base_model.eval()
+        self.n_classes = num_classes
+        hidden_size = 512
+        self.collapse = nn.LSTM(input_size=2048, hidden_size=hidden_size, num_layers=1, batch_first=True)
+        self.fc = nn.Linear(in_features=hidden_size, out_features=num_classes, bias=True)
+        self.num_views = num_views
+        self.first_pass = True
+
+    def forward(self, image_series):
+        # we run our images series through resnet and have them in a 5d tensor (batch, channels, width, height, series)
+        base_images = [] # each image is (batch,channels,width,height)
+        cbatch_size = image_series.shape[0]  #size of the current batch
+
+        self.base.eval()  #running means for batchnorm layers in non-trained base model - this is critical for efficient training
+        for j in range(0, cbatch_size):  #process each sequence separately (preparing for time when they may have different lengths)
+            features_sample = []
+            img_series_sample = image_series[j].float()
+            n_imgs = img_series_sample.shape[3]
+            img_series_sample = torch.unsqueeze(img_series_sample, dim=0)
+            img_series_sample_t = torch.transpose(img_series_sample, 0, 4)   #swap batch and sequence dimensions - all images in a sequence are fed into base as a pseudo-batch
+            img_series_sample_ts = torch.squeeze(img_series_sample_t)
+
+            out = self.base(img_series_sample_ts)
+
+            if j == 0:
+                base_images = out.reshape(1, n_imgs, -1)
+            else:
+                base_images = torch.cat([base_images, out.reshape(1, n_imgs, -1)], dim=0)
+
+        if base_images.is_cuda:
+            device = base_images.get_device()
+        else:
+            device = torch.device('cpu')
+
+        hidden_init = (torch.randn(1, cbatch_size, 512).to(device), torch.randn(1, cbatch_size, 512).to(device))
+        out, hidden = self.collapse(base_images, hidden_init)
+        pooled = self.fc(hidden[0].squeeze(dim=0))
+
+        return pooled
